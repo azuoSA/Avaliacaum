57d29fd93e797208191055e607aeff186a3e8aa6
ASD\allys <allysonsssouzas@gmail.com>
2021-09-13 16:38:08 -0300

    no message

---
+++ menu.py
@@ -0,0 +1,5 @@
+class main:
+    def main():
+        while True:
+            print('Selecione uma opção de validação: \n[1] Validar Com e-mail\n[2] Validar de CPF\n[3] Validar com CNPJ\n[4]Sair: \n\nOpção desejada: ')
+            opcaoDesejada = input()
